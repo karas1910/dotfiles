5611b885b2463c9eb4b6b98f0de696c239514729
sur1910 <https://github.com/sur1910>
2018-05-08 16:35:26 +0900

    modified init.el

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -31,23 +31,28 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     colors
+     ruby
+     c-c++
+     javascript
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     python
      helm
+     python
+     scala
      auto-completion
-     better-defaults
+     ;; better-defaults
      emacs-lisp
      git
      markdown
-     org
-     (shell :variables
-            shell-default-height 30
-            shell-default-position 'bottom)
-     ;; spell-checking
+     ;;org
+     ;; (shell :variables
+     ;;        shell-default-height 30
+     ;;        shell-default-position 'bottom)
+     ;;spell-checking
      syntax-checking
      version-control
      )
@@ -55,8 +60,17 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(quickrun
-                                      which-key)
+   dotspacemacs-additional-packages '(
+                                      bind-key
+                                      quickrun
+                                      migemo
+                                      impatient-mode
+                                      ace-isearch
+                                      ox-gfm
+                                      codic
+                                      all-the-icons
+                                      all-the-icons-dired
+                                      )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
@@ -128,14 +142,15 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light)
+   dotspacemacs-themes '(
+                         apropospriate-dark
+                         )
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
+                               :size 18
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
@@ -295,7 +310,9 @@ values."
    dotspacemacs-whitespace-cleanup nil
    ))
 
-(defun dotspacemacs/user-init ()
+(defun dotspacemacs/user-init (
+
+                               )
   "Initialization function for user code.
 It is called immediately after `dotspacemacs/init', before layer configuration
 executes.
@@ -305,36 +322,43 @@ before packages are loaded. If you are unsure, you should try in setting them in
   )
 
 (defun dotspacemacs/user-config ()
+  (global-set-key (kbd "C-;") 'evil-escape)
+  ;; 画面最大化
+  (toggle-frame-fullscreen)
+  ;; 行番号
+  (global-linum-mode t)
+  (global-set-key [f6] 'linum-mode)
+  (setq linum-format "%4d ")
+  ;; quickrun
+  (push '("*quickrun*") popwin:special-display-config)
+  (global-set-key (kbd "<f1>") 'quickrun)
+  (global-set-key (kbd "C-<f1>") 'quickrun-with-arg)
+  ;; インデントハイライト
+;;  (add-hook 'python-mode-hook 'highlight-indentation-mode)
+;;  (add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
+;;  (set-face-background 'highlight-indentation-face "#111111")
+;;  (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
   "Configuration function for user code.
 This function is called at the very end of Spacemacs initialization after
 layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-
-  ;; use company for auto-complete every mode
-  (global-company-mode)
-
-  ;; escape -> C-;
-  (global-set-key (kbd "C-;") 'evil-escape)
-  ;; 画面最大化
-  (toggle-frame-maximized)
-   ;; keybinding
-   (global-set-key (kbd "C-h") 'delete-backward-char)
-   (global-set-key (kbd "<F1>") help-map)
-   ;; 行番号
-   (global-linum-mode t)
-   (global-set-key [f6] 'linum-mode)
-   (setq linum-format "%4d ")
-   ;; quickrun
-   (push '("*quickrun*") popwin:special-display-config)
-   (global-set-key (kbd "<f1>") 'quickrun)
-   (global-set-key (kbd "C-<f1>") 'quickrun-with-arg)
-
-   (add-hook 'python-mode-hook 'highlight-indentation-mode)
-   (set-face-background 'highlight-indentation-face "#000000")
-
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ox-gfm disaster company-c-headers cmake-mode clang-format noflet ensime sbt-mode scala-mode \(setq-default\ dotspacemacs-themes\ \(quote\ \(apropospriate-dark\)\)\)-theme reveal-in-osx-finder rainbow-mode rainbow-identifiers pbcopy osx-trash osx-dictionary launchctl color-identifiers-mode quickrun migemo impatient-mode codic all-the-icons-dired all-the-icons memoize ace-isearch rvm ruby-tools ruby-test-mode rubocop rspec-mode robe rbenv rake minitest chruby bundler inf-ruby web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc company-tern tern coffee-mode yapfify smeargle pyvenv pytest pyenv-mode py-isort pip-requirements orgit org-projectile-pre-repo org-category-capture org-present org-pomodoro alert log4e gntp org-download mmm-mode markdown-toc markdown-mode magit-gitflow live-py-mode hy-mode dash-functional htmlize helm-pydoc helm-gitignore helm-company helm-c-yasnippet gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md fuzzy flyspell-correct-helm flyspell-correct flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit ghub let-alist with-editor diff-hl cython-mode company-statistics company-anaconda company auto-yasnippet yasnippet auto-dictionary anaconda-mode pythonic ac-ispell auto-complete ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
